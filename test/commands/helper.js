5cabdce8df62ef33c4c93cb311607ae5881de83c
Ben Bernard <github@benjaminbernard.com>
2020-11-18 18:53:58 -0800

    Partially working xform test

--- test/commands/helper.js
+++ test/commands/helper.js
@@ -1,5 +1,6 @@
 const commander = require("commander");
 const Command = prequire("lib/command");
+const stream = require("stream");
 
 exports.runCommand = function (args) {
   const program = new commander.Command();
@@ -7,4 +8,44 @@ exports.runCommand = function (args) {
   program.parse(["node", "script", ...args]);
 
   return program.lastCommandInstance;
+};
+
+exports.testifyCommand = function (klass) {
+  const spec = klass.spec;
+
+  class SubClass extends klass {
+    jsonOutputStream() {
+      const outputRecords = (this.outputRecords = []);
+
+      console.log("setting up test output stream");
+
+      const collectingStream = new stream.Writable({
+        objectMode: true,
+        write(obj) {
+          outputRecords.push(obj);
+          console.log("got record");
+        },
+      });
+
+      this.donePromise = new Promise((resolve, reject) => {
+        collectingStream.on("error", reject);
+        collectingStream.on("finish", () => {
+          console.log("end");
+          resolve(outputRecords);
+        });
+      });
+
+      return collectingStream;
+    }
+  }
+
+  before(() => {
+    Command.register(SubClass, spec, true);
+  });
+
+  after(() => {
+    Command.register(klass, spec, true);
+  });
+
+  return SubClass;
 };
--- test/commands/xform.js
+++ test/commands/xform.js
@@ -1,16 +1,22 @@
 const Xform = prequire("lib/commands/xformCommand"); // Must require in order to get it to register
 const Command = prequire("lib/command");
-const { runCommand } = require("./helper");
+const { runCommand, testifyCommand } = require("./helper");
 const { testFile } = prequire("test/testSetup");
 
 describe("Xform Command", () => {
-  it("should instantiate xform command", () => {
+  testifyCommand(Xform);
+
+  it("should instantiate xform command", async () => {
     const lastCommand = runCommand([
       "xform",
       'r.a = "foo"',
       testFile("simple.recs"),
     ]);
 
+    let records = await lastCommand.donePromise;
+    console.log("test", records);
+
     expect(lastCommand).to.be.instanceof(Xform);
+    expect();
   });
 });
