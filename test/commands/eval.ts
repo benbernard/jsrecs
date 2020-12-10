df0cc5b1155e39b88c40739534e003f657b7feda
Ben Bernard <github@benjaminbernard.com>
2020-12-10 11:15:47 -0800

    Adding eval tests

--- test/commands/eval.ts
+++ test/commands/eval.ts
@@ -18,36 +18,8 @@ describe("Eval Command", () => {
     ]);
   });
 
-  // it("should allow global variables", async () => {
-  //   await runCommand([
-  //     "xform",
-  //     "global.count ??= 0; count++; r.b = count",
-  //     testFile("simple.recs"),
-  //   ]);
-  //
-  //   expect(await result.records()).to.be.records([
-  //     { a: 1, b: 1 },
-  //     { a: 2, b: 2 },
-  //   ]);
-  // });
-  //
-  // it("should error once on snippet syntax error", async () => {
-  //   await expect(
-  //     runCommand(["xform", "r.a + ", testFile("simple.recs")])
-  //   ).to.be.rejectedWith(BailError, "Unexpected token");
-  //
-  //   expect(result.consoleLines).to.deep.equal([]);
-  // });
-  //
-  // it("should error for each record on runtime error", async () => {
-  //   await expect(runCommand(["xform", "r.a + b", testFile("simple.recs")])).to
-  //     .be.fulfilled;
-  //
-  //   expect(result.consoleLines).to.deep.equal([]);
-  //   expect(
-  //     result.logLines.error.map(str =>
-  //       str.includes("ReferenceError - b is not defined")
-  //     )
-  //   ).to.deep.equal([true, true]);
-  // });
+  it("should output numbers", async () => {
+    await runCommand(["eval", "return LINE", testFile("simple.recs")]);
+    expect(result.consoleLines).to.deep.equal(["1", "2"]);
+  });
 });
